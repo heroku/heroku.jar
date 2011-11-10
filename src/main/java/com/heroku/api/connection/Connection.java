2bcabb1f4e240bf28adb6752edcef74b18ee6f6e
Scott Clasen <scott@heroku.com>
2011-11-09 16:49:49 -0800

    remove httpclient from connection interface

--- src/main/java/com/heroku/api/connection/Connection.java
+++ src/main/java/com/heroku/api/connection/Connection.java
@@ -2,7 +2,6 @@ package com.heroku.api.connection;
 
 import com.heroku.api.command.Command;
 import com.heroku.api.command.CommandResponse;
-import org.apache.http.client.HttpClient;
 
 import java.io.IOException;
 import java.net.URL;
@@ -15,8 +14,6 @@ import java.net.URL;
 public interface Connection {
 
     <T extends CommandResponse> T executeCommand(Command<T> command) throws IOException;
-
-    HttpClient getHttpClient();
 
     URL getEndpoint();
 
