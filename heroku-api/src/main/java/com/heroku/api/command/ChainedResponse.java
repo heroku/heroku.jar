42c8acbaea9d7e636412bae2928008d28936c7b5
Scott Clasen <scott@heroku.com>
2011-11-14 15:01:01 -0800

    bit of doc for ChainedResponse

--- heroku-api/src/main/java/com/heroku/api/command/ChainedResponse.java
+++ heroku-api/src/main/java/com/heroku/api/command/ChainedResponse.java
@@ -1,6 +1,16 @@
 package com.heroku.api.command;
 
 
+/**
+ * A chained response is a response that returns a command that should subsequently be executed.
+ * <p/>
+ * For intstance to get access to logs, an initial LogsRequest is sent which sets up the logplex session
+ * and returns a LogResponse  (which is a ChainedResponse<TextCommand>).
+ * Executing the command returned from the LogResponse will
+ * actually be the command that gets log data for you.
+ *
+ * @param <T> The type of the 'next' command that should be executed.
+ */
 public class ChainedResponse<T extends Command> implements CommandResponse {
 
     T nextCommand;
