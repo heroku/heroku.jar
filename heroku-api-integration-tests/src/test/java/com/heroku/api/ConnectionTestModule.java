6dd67a4240eaa25d2b0ae1d614b2bda60438e765
Scott Clasen <scott@heroku.com>
2011-11-15 11:33:12 -0800

    remove auth.properties from test module, replace with TestConfig enum which reads from sys props or env vars

--- heroku-api-integration-tests/src/test/java/com/heroku/api/ConnectionTestModule.java
+++ heroku-api-integration-tests/src/test/java/com/heroku/api/ConnectionTestModule.java
@@ -5,8 +5,8 @@ import com.google.inject.Provides;
 import com.heroku.api.connection.Connection;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+
+import static com.heroku.api.TestConfig.*;
 
 /**
  * Guice Module for providing tests with authentication information.
@@ -22,22 +22,10 @@ public abstract class ConnectionTestModule extends AbstractModule {
 
     abstract Connection<?> getConnection() throws IOException;
 
-    @Provides
-    Properties getProperties() throws IOException {
-        Properties properties = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("auth-test.properties");
-        properties.load(inputStream);
-        return properties;
-    }
 
     @Provides
     AuthenticationTestCredentials getCredentials() throws IOException {
-        Properties props = getProperties();
-        return new AuthenticationTestCredentials(
-                props.getProperty("endpoint")
-                , props.getProperty("username")
-                , props.getProperty("password")
-        );
+        return new AuthenticationTestCredentials(ENDPOINT.getRequiredConfig(), USER.getRequiredConfig(), PASSWORD.getRequiredConfig());
     }
 
     public static class AuthenticationTestCredentials {
