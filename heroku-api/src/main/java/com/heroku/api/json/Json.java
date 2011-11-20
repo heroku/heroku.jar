13962a35ad1902ffdfcbb559d0127aa7a1155a54
Scott Clasen <scott@heroku.com>
2011-11-19 23:56:09 -0800

    switch to init on demand pattern

--- heroku-api/src/main/java/com/heroku/api/json/Json.java
+++ heroku-api/src/main/java/com/heroku/api/json/Json.java
@@ -6,29 +6,22 @@ import java.util.ServiceLoader;
 
 public class Json {
 
-    private static volatile boolean initialized = false;
-    private static JsonParser parser;
-    private static Object lock = new Object();
+    private static class Holder {
+        static JsonParser parser;
 
-    public static JsonParser getJsonParser() {
-        if (parser != null) return parser;
-        if (!initialized) {
-            synchronized (lock) {
-                if (!initialized) {
-                    ServiceLoader<JsonParser> loader = ServiceLoader.load(JsonParser.class);
-                    Iterator<JsonParser> iterator = loader.iterator();
-                    if (iterator.hasNext()) {
-                        parser = iterator.next();
-                    } else {
-                        throw new IllegalStateException("Unable to load a JSONProvider, please make sure you have a com.heroku.api.json.JSONParser implementation" +
-                                "on your classpath that can be discovered and loaded via java.util.ServiceLoader");
-                    }
-                }
+        static {
+            ServiceLoader<JsonParser> loader = ServiceLoader.load(JsonParser.class);
+            Iterator<JsonParser> iterator = loader.iterator();
+            if (iterator.hasNext()) {
+                parser = iterator.next();
+            } else {
+                throw new IllegalStateException("Unable to load a JSONProvider, please make sure you have a com.heroku.api.json.JSONParser implementation" +
+                        "on your classpath that can be discovered and loaded via java.util.ServiceLoader");
             }
-
         }
-        return parser;
     }
 
-
+    public static JsonParser getJsonParser() {
+        return Holder.parser;
+    }
 }
