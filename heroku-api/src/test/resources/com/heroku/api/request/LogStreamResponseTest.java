854c6b64b1e6f5ee0ac1fee4f0f24d3205f56c06
Scott Clasen <scott@heroku.com>
2011-11-30 15:32:20 -0800

    refactor test so that it dosent hang the build if there isnt any logplex output

--- heroku-api/src/test/resources/com/heroku/api/request/LogStreamResponseTest.java
+++ heroku-api/src/test/resources/com/heroku/api/request/LogStreamResponseTest.java
@@ -4,9 +4,7 @@ import com.heroku.api.Heroku;
 import com.heroku.api.request.log.LogStreamResponse;
 import org.testng.annotations.Test;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URL;
 
 public class LogStreamResponseTest {
@@ -14,12 +12,6 @@ public class LogStreamResponseTest {
     @Test
     public void testLogStream() throws IOException {
         LogStreamResponse response = new LogStreamResponse(new URL(Heroku.Config.ENDPOINT.value));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(response.openStream()));
-        String line = "";
-        while (line != null) {
-            System.out.println(line);
-            line = reader.readLine();
-        }
-
+        response.openStream().close();
     }
 }
