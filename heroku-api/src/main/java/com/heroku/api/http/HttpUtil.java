664584a40fe6ba1129248fe529a2032ad388957a
Scott Clasen <scott@heroku.com>
2011-12-22 11:20:44 -0800

    show the malformed url to aid in debugging

--- heroku-api/src/main/java/com/heroku/api/http/HttpUtil.java
+++ heroku-api/src/main/java/com/heroku/api/http/HttpUtil.java
@@ -61,7 +61,7 @@ public class HttpUtil {
         try {
             return new URL(url);
         } catch (MalformedURLException e) {
-            throw new RuntimeException("The URL was malformed");
+            throw new RuntimeException("The URL was malformed: "+url);
         }
     }
 
