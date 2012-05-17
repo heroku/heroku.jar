a84ad8fc3e99147776d5ae813ebab8e4fe8d6b92
Scott Clasen <scott@heroku.com>
2012-05-17 14:23:04 -0700

    Create the runtime instance of the jaxbContext in XmlParser once statically.
    These are threadsafe, and creating the same context multiple times is ineffecient and causes jaxb to generate throw and catch several exceptions.
    
    Fixes #33
    
    One thing to remeber here is that we need to add any classes with Jaxb annotations to the jaxbContext in XmlParser. But it is likely we wont add any more
    and will actually remove them.

--- heroku-api/src/main/java/com/heroku/api/parser/XmlParser.java
+++ heroku-api/src/main/java/com/heroku/api/parser/XmlParser.java
@@ -1,5 +1,9 @@
 package com.heroku.api.parser;
 
+import com.heroku.api.App;
+import com.heroku.api.Collaborator;
+import com.heroku.api.Domain;
+import com.heroku.api.User;
 import com.heroku.api.exception.ParseException;
 
 import javax.xml.bind.JAXBContext;
@@ -13,11 +17,21 @@ import java.lang.reflect.Type;
  * @author Naaman Newbold
  */
 public class XmlParser implements Parser {
+
+    static JAXBContext jaxbContext;
+
+    static {
+        try {
+            jaxbContext = JAXBContext.newInstance(App.class, Collaborator.class, Domain.class, User.class);
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     @SuppressWarnings("unchecked")
     public <T> T parse(byte[] data, Type type) {
         try {
-            JAXBContext jaxbContext = JAXBContext.newInstance((Class) type);
             return (T) jaxbContext.createUnmarshaller().unmarshal(new StringReader(new String(data).trim()));
         } catch (JAXBException e) {
             throw new ParseException(e);
