2d938959dfad18fb84608ab7d72493c11a29c999
Ryan Brainard <brainard@heroku.com>
2012-07-26 15:15:08 -0700

    add JerseyClientJsonParserTest

---
+++ heroku-json-jersey-client/src/test/java/com/heroku/api/parser/JerseyClientJsonParserTest.java
@@ -0,0 +1,20 @@
+package com.heroku.api.parser;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * @author Ryan Brainard
+ */
+public class JerseyClientJsonParserTest {
+
+    @Test
+    public void testParse() throws Exception {
+        Parser p = new JerseyClientJsonParser();
+        Something expected = new Something("coffee");
+        Something actual = p.parse("{\"name\":\"coffee\"}".getBytes(), Something.class);
+        assertEquals(actual, expected);
+    }
+}
