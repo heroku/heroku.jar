f8a36d790f5033320811d3cf47cc1775eeb5422d
Joe Kutner <jpkutner@gmail.com>
2017-01-23 09:36:32 -0600

    Remove release profile script

--- release.sh
+++ release.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-if [ -f .release.profile ]; then
- source .release.profile
-else
-  echo "Skipping release profile..."
-  if [ -z "$HEROKU_TEST_USERS" ]; then
-    echo "HEROKU_TEST_USERS is empty!"
-    exit 1
-  fi
-fi
-
 rm -f */pom.xml.releaseBackup
 rm -f pom.xml.releaseBackup
 rm -f release.properties
