89f19f61e08c407f32fba6adabfe2871969a1dba
Joe Kutner <jpkutner@gmail.com>
2017-06-16 11:44:02 -0500

    Unset HEROKU_TEST_USERS before release

--- release.sh
+++ release.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+unset HEROKU_TEST_USERS
+
 rm -f */pom.xml.versionsBackup && rm -f pom.xml.versionsBackup && rm -f */pom.xml.releaseBackup && rm -f pom.xml.releaseBackup && rm -f release.properties
 
 mvn release:prepare release:perform
